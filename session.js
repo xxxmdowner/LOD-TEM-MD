34713e1d64d425425c3acaf2658c530961d3ecc6
xxxmdowner <cyberdinu707@gmail.com>
2025-03-21 09:39:24 +0530

    Create session.js

---
+++ session.js
@@ -0,0 +1,40 @@
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                   𝗤𝗨𝗘𝗘𝗡-𝗔𝗡𝗝𝗨 𝗫ᴾᴿᴼ 𝐁𝐎𝐓                                               //
+//                                                                                                      //
+//                                         Ｖ：4.0                                                       //
+//
+//
+//                                           
+//
+//
+//
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//*
+//  * @project_name : © 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ
+//  * @version      : 4.0
+//  * @author       : Janith Rashmika
+//  * @youtube      : https://www.youtube.com/@gamingrash2006
+//  * @description  : © 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ, A Multi-functional WhatsApp bot created by Janith Rashmika.
+//*
+//*
+//Base by Janith Rashmika
+//GitHub: @Mrrashmika
+//WhatsApp: +94717775628
+//Want more free bot scripts? Subscribe to my YouTube channel: https://youtube.com/@gamingrash2006
+//   * Created By GitHub: Mrrashmika
+//   * Credit To Janith Rashmika
+//   * © 2025 © 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ-V4.
+// ⛥┌┤
+// */
+
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=WNFGBbpQ#qa-iTRCUoXWeLbK4rEXb7EisfArnvpo8xYk78jSYH8g",
+PORT: process.env.PORT || "8000"
+};
