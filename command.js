ba1ed52a2994c8038392bc16c7428f30cfac88fe
xxxmdowner <cyberdinu707@gmail.com>
2025-03-21 08:54:39 +0530

    Create command.js

---
+++ command.js
@@ -0,0 +1,30 @@
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                   𝗟𝗢𝗗-𝗧𝗘𝗠-𝗠𝗗                                               //
+//                                                                                                      //
+//                                         Ｖ：4.0                                                       //
+//
+// *`𝗟𝗢𝗗-𝗧𝗘𝗠-𝗠𝗗`*
+//
+//
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//*
+//  * @project_name : © 𝗟𝗢𝗗-𝗧𝗘𝗠-𝗠𝗗
+//  * @version      : 1.0
+//  * @author       : 𝗖𝗬𝗕𝗘𝗥 𝗗𝗜𝗡𝗨 𝗜𝗗
+//  * @youtube      : https://www.youtube.com/@..
+//  * @description  : © 𝙻𝙾𝙳-𝚃𝙴𝙼-𝙼𝙳, A Multi-functional WhatsApp bot created by 𝙲𝚈𝙱𝙴𝚁 𝙳𝙸𝙽𝚄 𝙸𝙳.
+//*
+//*
+//Base by 𝙲𝚈𝙱𝙴𝚁 𝙳𝙸𝙽𝚄 𝙸𝙳
+//GitHub: @xxxmdowner
+//WhatsApp: 94764972683
+//Want more free bot scripts? Subscribe to my YouTube channel: https://youtube.com/@...
+//   * Created By GitHub: CYBER DINU ID 
+//   * Credit To CYBER DINU ID 
+//   * © 2025 © ʟᴏᴅ-ᴛᴇᴍ-ᴍᴅ.
+// ⛥┌┤
+// */
+
+function _0x5e3c(_0x57ce6e,_0x53ffaf){var _0x1c2707=_0x1c27();return _0x5e3c=function(_0x5e3c38,_0xd6bd12){_0x5e3c38=_0x5e3c38-0xbf;var _0x3dd15a=_0x1c2707[_0x5e3c38];return _0x3dd15a;},_0x5e3c(_0x57ce6e,_0x53ffaf);}(function(_0x144065,_0xa2c29e){var _0x560585=_0x5e3c,_0x29208a=_0x144065();while(!![]){try{var _0x4a0e2c=parseInt(_0x560585(0xbf))/0x1+parseInt(_0x560585(0xc9))/0x2*(-parseInt(_0x560585(0xcc))/0x3)+parseInt(_0x560585(0xd0))/0x4+parseInt(_0x560585(0xd2))/0x5+-parseInt(_0x560585(0xcd))/0x6*(parseInt(_0x560585(0xc7))/0x7)+-parseInt(_0x560585(0xc3))/0x8*(-parseInt(_0x560585(0xc8))/0x9)+parseInt(_0x560585(0xcb))/0xa*(-parseInt(_0x560585(0xc0))/0xb);if(_0x4a0e2c===_0xa2c29e)break;else _0x29208a['push'](_0x29208a['shift']());}catch(_0x4a5fc8){_0x29208a['push'](_0x29208a['shift']());}}}(_0x1c27,0x1dc28));var commands=[];function cmd(_0x1c0d65,_0x3640a2){var _0x193a77=_0x5e3c,_0x59d3eb=_0x1c0d65;_0x59d3eb[_0x193a77(0xc6)]=_0x3640a2;if(!_0x59d3eb['dontAddCommandList'])_0x59d3eb[_0x193a77(0xc5)]=![];if(!_0x1c0d65[_0x193a77(0xd1)])_0x1c0d65[_0x193a77(0xd1)]='';if(!_0x59d3eb[_0x193a77(0xcf)])_0x59d3eb['fromMe']=![];if(!_0x1c0d65[_0x193a77(0xca)])_0x59d3eb[_0x193a77(0xca)]=_0x193a77(0xce);if(!_0x1c0d65['filename'])_0x59d3eb[_0x193a77(0xc2)]=_0x193a77(0xc4);return commands[_0x193a77(0xc1)](_0x59d3eb),_0x59d3eb;}module['exports']={'cmd':cmd,'AddCommand':cmd,'Function':cmd,'Module':cmd,'commands':commands};function _0x1c27(){var _0x49da3c=['misc','fromMe','422268irVFBH','desc','950330cGefos','138148WrziTL','22fEjBQO','push','filename','48DKMciK','Not\x20Provided','dontAddCommandList','function','247037hybpzZ','46836OWKRnH','20yRzmIb','category','764630vMyNmY','25293oXJNzk','18zaRLTK'];_0x1c27=function(){return _0x49da3c;};return _0x1c27();}
+
